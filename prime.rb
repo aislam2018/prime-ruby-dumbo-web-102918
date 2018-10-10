04c3477f273b20159262fca980e1fbb6c4acd217
anik1606 <anik1606@gmail.com>
2018-10-10 21:51:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,13 @@
 def prime?(num)
   if num < 2 
     return false;
+  end
     counter = 2 
     while counter < num 
     if num % counter == 0 
       return false 
-      counter += 1 
     end
+    counter += 1 
   end
     return true 
   end
