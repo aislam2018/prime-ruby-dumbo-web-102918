5f03e79310f360b02e1b4da189c3ac0e39751388
anik1606 <anik1606@gmail.com>
2018-10-10 21:49:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if num < 2 
+    return false;
+    counter = 2 
+    while counter < num 
+    if num % counter == 0 
+      return false 
+      counter += 1 
+    end
+    return true 
+  end
